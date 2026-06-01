b378b11d95f62632e4b288be2741ce01b7fb7180
cashen <54cashen@gmail.com>
2026-06-02 07:08:20 +0800

    3968

---
+++ ln-rank/js/shared/release-meta.v3968.js
@@ -0,0 +1,8 @@
+export const LN_RANK_RELEASE = {
+  version: 'v3.9.6.8',
+  code: 'v3968',
+  name: 'mobile-pool-entry-humanized',
+  appScript: 'app.v3968.js',
+  selectionPoolScript: 'selection-pool.v3968.js',
+  releaseDate: '2026-06-02'
+};
