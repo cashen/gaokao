3cdb3a504bd376947e8bb917f62709af1a5cd0b5
cashen <54cashen@gmail.com>
2026-06-15 03:17:01 +0800

    22

--- ln-rank/js/domain/version-contract.js
+++ ln-rank/js/domain/version-contract.js
@@ -1,5 +1,5 @@
 export const LN_RANK_VERSION = {
   display: 'v3.9.37',
   asset: '3937_0',
-  release: 'v3.9.37-ln-rank-clean-responsive-layout-refactor-preserve-function-no-fenxi'
+  release: 'v3.9.37-ln-rank-parent-guided-flow-visual-implementation-12-role-no-fenxi'
 };
