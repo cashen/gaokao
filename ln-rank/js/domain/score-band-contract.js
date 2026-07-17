ce7e1829fcd4161bc9e1f49590c3940043584d3d
cashen <54cashen@gmail.com>
2026-07-17 14:45:34 +0800

    chore(ln-rank): advance runtime cache contract to v3949_3

--- ln-rank/js/domain/score-band-contract.js
+++ ln-rank/js/domain/score-band-contract.js
@@ -1,6 +1,6 @@
-import { RANGE_PRESETS } from '../config/range-presets.js?v=3949_0';
-import { normalizeRangePreset } from './range-policy.js?v=3949_0';
-import { normalizeBandFocus } from './band-policy.js?v=3949_0';
+import { RANGE_PRESETS } from '../config/range-presets.js?v=3949_3';
+import { normalizeRangePreset } from './range-policy.js?v=3949_3';
+import { normalizeBandFocus } from './band-policy.js?v=3949_3';
 
 export const SCORE_BAND_KEYS = ['upper', 'near', 'steady'];
 
