d03bf2969f59eb26e891a570aebcd90a3ba05bce
cashen <54cashen@gmail.com>
2026-07-20 05:23:46 +0800

    fix: update Tongxue copy release wrapper to v1.1.4

--- tongxue-performance-v113.js
+++ tongxue-performance-v113.js
@@ -1,5 +1,5 @@
 import './tongxue-performance-v112.js?v=113';
 import { installShareMetadataStabilizer } from './tongxue-share-stabilizer-v113.js';
-import { installTongxueShare } from './tongxue-share-v113.js';
-installShareMetadataStabilizer('v1.1.3');
-installTongxueShare({ pageVersion: 'v1.1.3' });
+import { installTongxueShare } from './tongxue-share-v113.js?v=114';
+installShareMetadataStabilizer('v1.1.4');
+installTongxueShare({ pageVersion: 'v1.1.4' });
