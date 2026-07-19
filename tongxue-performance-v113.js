aef9a22f807fa9d84783dfa366d431cfddf667e9
cashen <54cashen@gmail.com>
2026-07-20 04:56:54 +0800

    fix: use portable Tongxue share module imports

--- tongxue-performance-v113.js
+++ tongxue-performance-v113.js
@@ -1,3 +1,3 @@
-import '/tongxue-performance-v112.js?v=113';
-import { installTongxueShare } from '/tongxue-share-v113.js';
+import './tongxue-performance-v112.js?v=113';
+import { installTongxueShare } from './tongxue-share-v113.js';
 installTongxueShare({ pageVersion: 'v1.1.3' });
