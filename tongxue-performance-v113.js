57ab891f0ca25ab4c67c57d0d0b45ea588945d16
cashen <54cashen@gmail.com>
2026-07-20 05:11:30 +0800

    fix: restore Tongxue v1.1.3 entry imports

---
+++ tongxue-performance-v113.js
@@ -0,0 +1,5 @@
+import './tongxue-performance-v112.js?v=113';
+import { installShareMetadataStabilizer } from './tongxue-share-stabilizer-v113.js';
+import { installTongxueShare } from './tongxue-share-v113.js';
+installShareMetadataStabilizer('v1.1.3');
+installTongxueShare({ pageVersion: 'v1.1.3' });
