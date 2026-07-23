935fe9975d0167254329f081281e2162cfe8ae34
cashen <54cashen@gmail.com>
2026-07-23 15:45:11 +0800

    Mount shared family shell from selection workspace

--- ln-rank/js/selection-pool.v3959_0.js
+++ ln-rank/js/selection-pool.v3959_0.js
@@ -1,3 +1,4 @@
+import '../../shared/ui/shell/family-shell.v3959_0.js?v=3959_0';
 import { CURRENT_RELEASE } from '../../shared/resources/release/current-release.js?v=3959_0';
 
 globalThis.__GAOKAO_SHARED_RESOURCES__ = Object.freeze({
