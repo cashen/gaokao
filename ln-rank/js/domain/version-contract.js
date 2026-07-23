2474fc492f9dece490b0a8137a5d3bf57ede8a31
cashen <54cashen@gmail.com>
2026-07-23 12:50:58 +0800

    Read browser version from shared release owner

--- ln-rank/js/domain/version-contract.js
+++ ln-rank/js/domain/version-contract.js
@@ -1,5 +1,9 @@
-export const LN_RANK_VERSION = {
-  display: 'v3.9.49.4',
-  asset: '3949_4',
-  release: 'v3.9.49.4-ln-rank-multi-terminal-human-journey-regression-12-role-no-fenxi'
-};
+import { CURRENT_RELEASE } from '../../../shared/resources/release/current-release.js';
+
+export const LN_RANK_VERSION = Object.freeze({
+  display: CURRENT_RELEASE.display,
+  asset: CURRENT_RELEASE.asset,
+  assetVersion: CURRENT_RELEASE.assetVersion,
+  release: CURRENT_RELEASE.release,
+  resourceOwnershipVersion: CURRENT_RELEASE.resourceOwnershipVersion
+});
