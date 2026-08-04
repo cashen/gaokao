6f5d8ca3f3a905f7693f976e6476d75b6a0c52d8
cashen <54cashen@gmail.com>
2026-08-04 11:10:04 +0800

    feat(rank): expose compact pagination candidate transport

--- functions/_lib/major-bands-response-transport.v3990_0.js
+++ functions/_lib/major-bands-response-transport.v3990_0.js
@@ -1,4 +1,5 @@
 export {
+  compactMajorBandsBucketCandidate,
   compactMajorBandsResponseRecord,
   compactMajorBandsCanonicalPosition,
   compactMajorBandsHistoryEvidence,
@@ -6,4 +7,3 @@ export {
 } from './major-bands-bucket-transfer.v3972_5.js';
 
 export const MAJOR_BANDS_RESPONSE_TRANSPORT_VERSION = 'major-bands-response-compact-v3990_0';
-
